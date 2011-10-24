c76559c4da350926a23bacb84be2cbb211bec7f0
iiotep9huy <iiotep9huy@d5055edb-a196-7347-8869-490c5147d47b>
2011-10-24 07:12:59 +0000

    +bin

--- trunk/mipt/lessons/Fibonacci-heap/fibonacci_heap.hpp
+++ trunk/mipt/lessons/Fibonacci-heap/fibonacci_heap.hpp
@@ -1,22 +1,48 @@
 #include <vector>
 
-template<typename Key, typename Compare>
+#define CLASS_T \
+        template<typename Key, class Compare>
+
+#define CLASS_C \
+        fibonacci_heap<Key, Compare>
+
+template<typedef Data, typedef Key>
+class node
+{
+  Key key;
+  typedef typename std::vector<node>::iterator node_pointer;
+  node_pointer l, r, p, ch;
+  int degree;
+  bool mark;
+
+  node();
+  node(Key k)
+  {
+    l = r = p = ch = 0;
+    key = k;
+  }
+};
+
+template<typename Key, class Compare>
 class fibonacci_heap
 {
-  protected:
 
-  std::vector <Key> heap;
 
   public:
 
-  typedef typename std::vector<Key>::iterator node_pointer;
+  typedef typename std::vector<node>::iterator node_pointer;
 
   void push(Key k);
 
-  Key top();
-  
-  void erase(node_pointer pnode);
+  void merge(node_pointer pnode);
 
-  void merge(fibonacci_heap H1, fibonacci_heap H2);
+  Key top();
+
+
+  protected:
+  std::vector<node> heap;
+  node min_H;
 
 };
+
+
