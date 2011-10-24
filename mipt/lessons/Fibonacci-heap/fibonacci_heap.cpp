2a5df01ed0eab45c29f417ee5b77bf7c2014de7e
iiotep9huy <iiotep9huy@d5055edb-a196-7347-8869-490c5147d47b>
2011-10-24 07:12:59 +0000

    +bin

--- mipt/lessons/Fibonacci-heap/fibonacci_heap.cpp
+++ mipt/lessons/Fibonacci-heap/fibonacci_heap.cpp
@@ -1,2 +1,24 @@
+#include "fibonacci_heap.hpp"
 
+CLASS_T
+void CLASS_C::push(Key k)
+{
+  heap.push_back(node(k));
+  node_pointer pnode = heap.end();
+  this->merge(pnode);
+}
+
+CLASS_T
+void CLASS_C::merge(node_pointer pnode)
+{
+  pnode->r = min_H -> r;
+  min_H->r->l = pnode;
+  min_H->r = pnode;
+}
+
+CLASS_T
+Key CLASS_C::top()
+{
+  return min_H.key;
+}
 
