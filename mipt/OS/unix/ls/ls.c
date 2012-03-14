f6f9bd9efb15e737e981dacd0a9cf19fad28e25c
iiotep9huy <iiotep9huy@d5055edb-a196-7347-8869-490c5147d47b>
2012-03-14 12:52:50 +0000

    unix solutions added

--- mipt/OS/unix/ls/ls.c
+++ mipt/OS/unix/ls/ls.c
@@ -2,65 +2,147 @@
 #include <stdio.h>
 #include <string.h>
 #include <dirent.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <time.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <pwd.h>
+#include <grp.h>
+#include <stdbool.h>
 
-#define MAX_DEPTH 10
-#define MAX_PATH_LENGTH (MAX_DEPTH * (1 << 8))
+#define MAX_DEPTH = 10
 
-//const int MAX_DEPTH = 10;
-//const int MAX_PATH_LENGTH = 255 * MAX_DEPTH;
+const char DIR_SEPARATOR = '/';
+const char* DIR_SEPARATOR_STR = "/";
 
-char path[MAX_PATH_LENGTH];
-int max_depth = 0;
+int max_depth = 100;
 
-int ValidFile(char* name)
+bool isValidFileName(char* name)
 {
-	if (name == NULL)
-		return 0;
-	if (name[0] == '.')
-		return 0;
-	return 1;
+  if (name == NULL || name[0] == '.')
+    return false;
+  return true;
 }
 
-void ls(char* absolute_path, int depth)
+void printFileMode(mode_t file_mode)
+{
+  printf("%c", S_ISDIR(file_mode) ? 'd' : '-');
+
+  printf("%c", S_IRUSR & file_mode ? 'r' : '-');
+  printf("%c", S_IWUSR & file_mode ? 'w' : '-');
+  printf("%c", S_IXUSR & file_mode ? 'x' : '-');
+
+  printf("%c", S_IRGRP & file_mode ? 'r' : '-');
+  printf("%c", S_IWGRP & file_mode ? 'w' : '-');
+  printf("%c", S_IXGRP & file_mode ? 'x' : '-');
+
+  printf("%c", S_IROTH & file_mode ? 'r' : '-');
+  printf("%c", S_IWOTH & file_mode ? 'w' : '-');
+  printf("%c", S_IXOTH & file_mode ? 'x' : '-');
+}
+
+void printFileStats(char* file_path, char* file_name, int detailed)
+{
+  if (detailed)
+  {
+    struct stat file_stat;
+    if (stat(file_path, &file_stat) == 0)
+    {
+      printFileMode(file_stat.st_mode);
+      struct passwd* pwd = getpwuid(file_stat.st_uid);
+      struct group* grp = getgrgid(file_stat.st_gid);
+      char* time = ctime(&file_stat.st_atime);
+      time[strlen(time) - 1] = '\0';
+      printf(" %1d %6s %5s %5d %3s %s\n", file_stat.st_nlink,
+                                          pwd->pw_name,
+                                          grp->gr_name, 
+                                          file_stat.st_size, 
+                                          time,
+                                          file_name);
+    }
+    else
+      printf("problem %s\n", file_path); // this should never happen
+  }
+  else
+  {
+    printf("%s ", file_name);
+  }
+}
+
+bool isExistingPath(char* absolute_path)
+{
+  DIR* dir_file = opendir(absolute_path);
+  if (dir_file == NULL)
+  {
+    //printf("Error: Path does not exits.\n");
+    return false;
+  }
+  closedir(dir_file);
+  return true;
+}
+
+void ls(char* absolute_path, int depth, int detailed)
 {
   if (depth > max_depth)
     return;
-	DIR* dir_file = opendir(absolute_path);
-	if (dir_file == NULL)
-		return;
+  if (!isExistingPath(absolute_path))
+    return;
 
-	printf("%s:\n", absolute_path);
-	struct dirent* dir = readdir(dir_file);
-  char* new_path = (char*) malloc( sizeof(absolute_path) );
-	while(dir)
-	{
-    if (sizeof(new_path) < sizeof(absolute_path) + sizeof(dir->d_name))
-      new_path = (char*) realloc( new_path, sizeof(absolute_path) + sizeof(dir->d_name) );
-    new_path[0] = 0;
-		if (ValidFile(dir->d_name))
-		{
-			strcat(new_path, absolute_path);
-			strcat(new_path, dir->d_name);
-			printf("%s\n", dir->d_name);
-			ls(new_path, depth + 1);
-		}
-		dir = readdir(dir_file);
-	}
-  free(new_path);
-  free(dir);
+  printf("\n%s:\n", absolute_path);
+  size_t path_length = strlen(absolute_path);
+  char*  file_path   = (char*) malloc(path_length + 1);
+
+  strcpy(file_path, absolute_path);
+  if (file_path[path_length - 1] != DIR_SEPARATOR)
+  {
+    strcat(file_path, DIR_SEPARATOR_STR);
+    path_length++;
+  }
+  
+  for(int process_mode = 0; process_mode <= 1; process_mode++) // 0 - print, 1 - discover.
+  {
+    DIR*   dir_file    = opendir(absolute_path);
+    struct dirent* dir = readdir(dir_file);
+    while(dir)
+    {
+      if (isValidFileName(dir->d_name))
+      {
+        if (strlen(file_path) < path_length + strlen(dir->d_name))
+          file_path = (char*) realloc(file_path, path_length + strlen(dir->d_name) + 1);
+        file_path[path_length] = '\0';
+        strcat(file_path, dir->d_name);
+
+        if (process_mode == 0)
+        {
+          printFileStats(file_path, dir->d_name, detailed);
+        }
+        else
+        {
+          ls(file_path, depth + 1, detailed);
+        }
+      }
+      dir = readdir(dir_file);
+    }
+    closedir(dir_file);
+    free(dir);
+  }
+  free(file_path);
 }
 
 int main(int argc, char** argv)
 {
-  // -R all
-  // -maxdepth
-	if (argc < 2)
-	{
-		printf("Error: Need more arguments.\n");
-		return 1;
-	}
-  strcat(path, argv[1]);
-	ls(path, 0);
-	return 0;
+  if (argc < 2)
+  {
+    ls(".", 0, 0);
+    printf("\n");
+  }
+  else
+  {
+    for(int i = 1; i < argc; i++)
+    {
+      ls(argv[i], 0, 1);
+    }
+  }
+  return 0;
 }
