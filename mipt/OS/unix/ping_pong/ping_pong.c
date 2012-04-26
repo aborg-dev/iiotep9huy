69380a5c05a90d6a1f8af5f4b77dd8f61ac16576
iiotep9huy <iiotep9huy@d5055edb-a196-7347-8869-490c5147d47b>
2012-04-26 18:39:50 +0000

    ping_pong raw readded

--- mipt/OS/unix/ping_pong/ping_pong.c
+++ mipt/OS/unix/ping_pong/ping_pong.c
@@ -28,9 +28,11 @@
 #include <memory.h>
 
 #define REPORT(message) fprintf(stderr, "%d: %s\n", getpid(), message);
+#define REPORT2(s1, s2) fprintf(stderr, "%d: %s %s\n", getpid(), s1, s2);
 
-const size_t MESSAGE_MAXLEN = 20;
+const size_t MESSAGE_MAXLEN     = 20;
 const size_t SEMAPHORE_NAME_LEN = 15;
+const size_t ITERATIONS         = 3;
 
 const char* FIFO_NAME       = "/tmp/ping_pong_fifo";
 const char* DELETE_SEM_NAME = "delete_semaphore";
@@ -40,51 +42,47 @@ typedef struct
 {
   sem_t* sem;
   char* name;
-} named_semaphore;
+} named_sem;
 
-void introduction(named_semaphore* my_sem,
-                  named_semaphore* partner_sem,
+void introduction(named_sem* my_sem,
+                  named_sem* partner_sem,
                   sem_t* file_sem, 
                   int* fd,
                   char** buf)
 {
-  REPORT("I'm waiting for file");
-
   sem_wait(file_sem);
   *fd = mkfifo(FIFO_NAME, 0777);
   sem_post(file_sem);
 
   if (*fd == 0)
   {
+    REPORT("I've created file");
     *fd = open(FIFO_NAME, O_RDWR);
 
     sem_wait(file_sem);
-    REPORT("I created file");
     write(*fd, my_sem->name, SEMAPHORE_NAME_LEN); 
     sem_post(file_sem);
 
-    REPORT("I'm waiting for my_sem");
     sem_wait(my_sem->sem);
-
-    read(*fd, partner_sem->name, SEMAPHORE_NAME_LEN);
-    partner_sem->sem = sem_open(partner_sem->name, 0);
-    memset(*buf, 0, sizeof(*buf));
-    sprintf(*buf, "Hi, My name is %d", getpid());
-    write(*fd, *buf, MESSAGE_MAXLEN);
-    REPORT("Message was written");
-
+    {
+      read(*fd, partner_sem->name, SEMAPHORE_NAME_LEN);
+      partner_sem->sem = sem_open(partner_sem->name, 0);
+      sprintf(*buf, "Hello from %d", getpid());
+      write(*fd, *buf, MESSAGE_MAXLEN);
+    }
     sem_post(partner_sem->sem);
   }
   else
   {
     *fd = open(FIFO_NAME, O_RDWR);
-    //fprintf(stderr, "I am here2 %d\n", *fd);
-    sem_wait(file_sem);
     REPORT("I've opened file!");
-    read(*fd, partner_sem->name, SEMAPHORE_NAME_LEN);
-    REPORT(partner_sem->name)
-    partner_sem->sem = sem_open(partner_sem->name, 0);
-    write(*fd, my_sem->name, SEMAPHORE_NAME_LEN);
+
+    sem_wait(file_sem);
+    {
+      read(*fd, partner_sem->name, SEMAPHORE_NAME_LEN);
+      partner_sem->sem = sem_open(partner_sem->name, 0);
+      write(*fd, my_sem->name, SEMAPHORE_NAME_LEN);
+    }
     sem_post(file_sem);
     sem_post(partner_sem->sem);
   }
@@ -97,21 +95,34 @@ void converse(sem_t* my_sem,
 {
     fprintf(stderr, "\n");
     sem_wait(my_sem);
-    memset(*buf, 0, sizeof(*buf));
-    read(fd, *buf, MESSAGE_MAXLEN);
-    REPORT("I've got message: ");
-    REPORT(*buf);
-    memset(*buf, 0, sizeof(*buf));
-    sprintf(*buf, "Hi, my name is %d", getpid());
-    write(fd, *buf, MESSAGE_MAXLEN);
+    {
+      read(fd, *buf, MESSAGE_MAXLEN);
+
+      REPORT2("I've got message: ", *buf);
+      sprintf(*buf, "Hello from %d", getpid());
+
+      write(fd, *buf, MESSAGE_MAXLEN);
+    }
     sem_post(partner_sem);
-    sleep(5);
+    sleep(2);
 }
+
+void begin_conversation(int iterations, sem_t** my_sem, sem_t** partner_sem, 
+                        int fd, char** buf)
+{
+  while (iterations--)
+  {
+    converse(*my_sem, 
+             *partner_sem, 
+             fd, buf);
+  }
+}
+
 
 int main(int argc, char** argv)
 {
-  named_semaphore my_sem;
-  named_semaphore partner_sem;
+  named_sem my_sem;
+  named_sem partner_sem;
   my_sem.name = (char*) malloc(SEMAPHORE_NAME_LEN);
   partner_sem.name = (char*) malloc(SEMAPHORE_NAME_LEN);
   char* buf = (char*) malloc(MESSAGE_MAXLEN);
@@ -124,21 +135,10 @@ int main(int argc, char** argv)
 
   introduction(&my_sem, &partner_sem, file_sem, &fd, &buf);
 
-  int count = 3;
-  REPORT("Partner semaphore");
-  REPORT(partner_sem.name);
-  REPORT("I'm near inf while");
-  while (count--)
-  {
-    converse(my_sem.sem, 
-             partner_sem.sem, 
-             fd, &buf);
-  }
-  //int sem_value = 0;
-  //sem_getvalue(file_sem, &sem_value);
-  //fprintf(stderr, "Sem value: %d\n", sem_value);
-  REPORT("Deleting stuff");
-  //free(buf);
+  REPORT2("Partner semaphore: ", partner_sem.name);
+  begin_conversation(ITERATIONS, &my_sem.sem, &partner_sem.sem, fd, &buf);
+  
+  free(buf);
   free(my_sem.name);
   free(partner_sem.name);
   sem_unlink(my_sem.name);
